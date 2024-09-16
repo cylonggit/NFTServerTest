22659522f1c79987638093887c4ade19f074c56b
cylonggit <au033@126.com>
2024-09-16 21:08:23 +0800

    建立测试工程模板，采用springboot 的junit

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -16,17 +16,18 @@ public class TestExample {
 //    @Autowired
 //    FiscoBcosClient myFiscoClient;
 
-    @Before
+    @Before //为了测试预务好链上、mysql、minio、文件等数据
     public void init(){
         System.out.println("...int the env before the test....");
     }
 
-    @Test
+    @Test //测试功能，并与预期结果进行对比后并断言，断言使用asset
     public void testSomething() {
         System.out.println("...do the test here, including the assertion...");
+        assert (1==1);
    }
 
-   @After
+   @After  //主要是清理一些为了测试而弄出来的东西，如链上的NFT烧掉、mysql中的东西删除，或Minio的图片删掉等
     public void afterTest(){
        System.out.println("...if need to do something after the test, you can do here....");
     }
