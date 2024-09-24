b948b1f7d11493c7ea335ff9a3fa54c2267e92f8
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-24 23:25:47 +0800

    Fix 4

---
+++ src/test/java/CreateTest.java
@@ -0,0 +1,17 @@
+import com.market.bc.TestApplication;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TestApplication.class)
+public class CreateTest {
+    // 用于测试铸造NFT
+
+    @Before
+    public void init() {
+
+    }
+
+}
