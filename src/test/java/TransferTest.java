a25dbeb10dd57bdbba14180a698974e1c58c13af
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-25 16:19:40 +0800

    Fix

--- src/test/java/TransferTest.java
+++ src/test/java/TransferTest.java
@@ -1,15 +1,46 @@
 import com.market.bc.TestApplication;
+import com.market.bc.configurer.MyConfig;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestApplication.class)
 public class TransferTest {
 
-    @Before
-    public void init() {
+    @Autowired
+    MyConfig myConfig;
 
+    RestTemplate restTemplate = new RestTemplate();
+
+    String userID1, userID2;
+
+    @Before
+    public void initUser1() {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "mytest");
+        params.put("password", "123456");
+        ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/login", params, Map.class);
+        userID1 = ((Map) response.getBody().get("data")).get("userID").toString();
+        System.out.println(userID1);
     }
+
+    @Before
+    public void initUser2() {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "othertest");
+        params.put("password", "123456");
+        ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/login", params, Map.class);
+        userID2 = ((Map) response.getBody().get("data")).get("userID").toString();
+        System.out.println(userID2);
+    }
+
+
 }
