6adacc3e3b6845f4c3ce670782057391bc6d25f4
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-25 17:32:13 +0800

    Fix

--- src/test/java/TransferTest.java
+++ src/test/java/TransferTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,7 +22,7 @@ public class TransferTest {
 
     RestTemplate restTemplate = new RestTemplate();
 
-    String userID1, userID2;
+    String userID1, userID2, nftID;
 
     @Before
     public void initUser1() { // 初始化原所有者用户
@@ -35,10 +36,12 @@ public class TransferTest {
         // 找到原所有者用户的一件NFT
         Map<String, String> params1 = new HashMap<>();
         params1.put("page", "1");
-        params1.put("size", "10");
+        params1.put("size", "1");
         params1.put("userID", userID1);
         ResponseEntity<Map> response1 = restTemplate.getForEntity(myConfig.getBackendServerUrl() + "/nft/nft/ownNFT?page={page}&size={size}&userID={userID}", Map.class, params1);
         System.out.println(response1.getBody());
+        nftID = ((Map) ((List) ((Map) response1.getBody().get("data")).get("rows")).get(0)).get("nftID").toString();
+        System.out.println(nftID);
     }
 
     @Before
