c4e17ab37bafc597d380750fe6467e45fd6c0067
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-25 10:25:32 +0800

    Fix

--- src/test/java/CreateTest.java
+++ src/test/java/CreateTest.java
@@ -1,23 +1,39 @@
 import com.market.bc.TestApplication;
+import com.market.bc.configurer.MyConfig;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestApplication.class)
 public class CreateTest {
     // 用于测试铸造NFT
 
+    RestTemplate restTemplate = new RestTemplate();
+
     @Autowired
-    RestTemplate restTemplate;
+    MyConfig myConfig;
 
     @Before
     public void init() {
         // 用户联盟链账户已创建并拥有相应的权限
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "mytest");
+        params.put("password", "123456");
+        params.put("mobile", "15972133907");
+        params.put("email", "test@test.com");
+        restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/register", params, String.class);
+    }
 
+    @Test
+    public void test() {
 
     }
 
