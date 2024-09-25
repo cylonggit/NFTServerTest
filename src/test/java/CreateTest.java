eef56f26dacec28bf4a10d8c5fd30f2502a00b4f
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-25 11:28:41 +0800

    Fix

--- src/test/java/CreateTest.java
+++ src/test/java/CreateTest.java
@@ -5,9 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,14 +32,28 @@ public class CreateTest {
     MyConfig myConfig;
 
     @Before
-    public void init() {
+    public void init() throws URISyntaxException {
         // 用户联盟链账户已创建并拥有相应的权限
         Map<String, String> params = new HashMap<>();
         params.put("username", "mytest");
         params.put("password", "123456");
-        params.put("mobile", "15972133907");
-        params.put("email", "test@test.com");
-        restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/register", params, String.class);
+        ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/login", params, Map.class);
+        String userID = ((Map) response.getBody().get("data")).get("userID").toString();
+        System.out.println(userID);
+        // 获取userID
+
+        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
+        Path path = Paths.get(getClass().getClassLoader().getResource("images/2.jpg").toURI());
+        bodyMap.add("file", new FileSystemResource(path.toFile()));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
+        ResponseEntity<Map> response1 = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/file/file/upload", requestEntity, Map.class);
+        String url = response1.getBody().get("url").toString();
+        System.out.println(url);
+        // 上传图片
+
+
     }
 
     @Test
