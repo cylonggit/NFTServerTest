f9999dcc37283bf7ee084eccc6206e2022fb8c8b
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-26 09:26:18 +0800

    Fix

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,7 @@
 import com.market.bc.TestApplication;
 import com.market.bc.configurer.MyConfig;
+import com.market.bc.pojo.User;
+import entity.Result;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +54,23 @@ public class UserTest {
                 "TdUU6hellxMb5N4pRQIDAQAB\n" +
                 "-----END PUBLIC KEY-----\n");
         ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/register", params, Map.class);
+
+        assertEquals(20000, response.getBody().get("code"));
+        assertEquals("注册成功", response.getBody().get("msg"));
+
+        Map<String, String> params1 = new HashMap<>();
+        params1.put("username", "mytest" + random12DigitNumber);
+        params1.put("password", "123456");
+        ResponseEntity<Map> response1 = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/login", params1, Map.class);
+        String userID = ((Map) response1.getBody().get("data")).get("userID").toString();
+
+        params = new HashMap<>();
+        params.put("userID", userID);
+        ResponseEntity<Result> response0 = restTemplate.getForEntity(myConfig.getBackendServerUrl() + "/user/user?userID={userID}", Result.class, params);
+        System.out.println(response0.getBody().getData());
+        assertNotNull(new User((Map) response0.getBody().getData()).getAccountAddress());
+        assertNotNull(new User((Map) response0.getBody().getData()).getHexPrivateKey());
+        assertNotNull(new User((Map) response0.getBody().getData()).getNftRSAPublicKey());
     }
 
 }
