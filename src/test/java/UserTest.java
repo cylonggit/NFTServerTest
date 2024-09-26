36774336c13ed39be9db56c7d3183564024b12b5
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-26 08:53:47 +0800

    Fix

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,37 @@
+import com.market.bc.TestApplication;
+import com.market.bc.configurer.MyConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TestApplication.class)
+public class UserTest {
+    @Autowired
+    MyConfig myConfig;
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void testLogin() {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "mytest");
+        params.put("password", "123456");
+        ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/login", params, Map.class);
+        System.out.println(response.getBody());
+        assertEquals(20000, response.getBody().get("code"));
+        assertEquals("登录成功", response.getBody().get("msg"));
+        assertEquals("mytest", ((Map) response.getBody().get("data")).get("username"));
+        assertNotNull(((Map) response.getBody().get("data")).get("token"));
+    }
+}
