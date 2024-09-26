46f0c563d977ff94ff09f86fa0ca794cb8ce772f
legado <55886143+VoidxHoshi@users.noreply.github.com>
2024-09-26 09:10:11 +0800

    Fix

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -34,4 +35,23 @@ public class UserTest {
         assertEquals("mytest", ((Map) response.getBody().get("data")).get("username"));
         assertNotNull(((Map) response.getBody().get("data")).get("token"));
     }
+
+    @Test
+    public void testRegister() {
+        Random random = new Random();
+        String random12DigitNumber = String.format("%012d", random.nextInt());
+        Map<String, String> params = new HashMap<>();
+        params.put("username", "mytest" + random12DigitNumber);
+        params.put("password", "123456");
+        params.put("mobile", "131" + random12DigitNumber);
+        params.put("email", "mytest" + random12DigitNumber + "@test.com");
+        params.put("nftRSAPublicKey", "-----BEGIN PUBLIC KEY-----\n" +
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCy0spxQQtOzaviqyD8SPOxkUmv\n" +
+                "pgOfvYzITcIBSqZZ34AqrA1BscmW5M2oba7RMWRPpYY17KzgV1QvnfeNDVl1vmBQ\n" +
+                "t55EYUCUh7JwA+XF9JMb0g2zi/IrlhAqEaitZOq3En7GVRbAYVbZ4CX2F3A+EYjj\n" +
+                "TdUU6hellxMb5N4pRQIDAQAB\n" +
+                "-----END PUBLIC KEY-----\n");
+        ResponseEntity<Map> response = restTemplate.postForEntity(myConfig.getBackendServerUrl() + "/user/user/register", params, Map.class);
+    }
+
 }
